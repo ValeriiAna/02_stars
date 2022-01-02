adf27501e4d7ee77428b2b7b0bd0a6faeb95a9d6
ValeriiAna <95766059+ValeriiAna@users.noreply.github.com>
2022-01-02 14:23:29 +0200

    Add files via upload

--- figures.js
+++ figures.js
@@ -57,7 +57,7 @@ for (let i = 1; i <= n; i++) {
         }
         else {
             if (j === 0 || j === i - 1) {
-                string += "* ";
+                string += "*  ";
             }
             else {
                 string += "   ";
@@ -68,6 +68,88 @@ for (let i = 1; i <= n; i++) {
 }
 console.log(string)
 
+// Task - 5
+    let str = '';
+    for(let i = 0; i < 7; i++) {
+        for(let j = 0; j < 7; j++) {
+            if( 6 === (j + i) || j === 0) {
+                str = str + ' * ';
+            } else if( i === 0) {
+                str = str + ' * ';
+            } else {
+                str = str + '   ';
+            }
+        }
+        str = str + '\n';
+    }
+    console.log(str);
+
+// Task - 6
+    let str1 = '';
+    for(let i = 0; i < 7; i++) {
+        for(let j = 0; j < 7; j++) {
+            if( i === j || j === 6) {
+                str1 = str1 + ' * ';
+            } else if( i === 0) {
+                str1 = str1 + ' * ';
+            } else {
+                str1 = str1 + '   ';
+            }
+        }
+        str1 = str1 + '\n';
 
 
+}
+console.log(str1);
+
+// Task - 7
+    let str2 = '';
+    for(let i = 1; i <= 7; i++) {
+        for(let j = 0; j < 7; j++) {
+            if( 7 === (j + i) || j === 6) {
+                str2 = str2 + ' * ';
+            } else if( i === 7) {
+                str2 = str2 + ' * ';
+            } else {
+                str2 = str2 + '   ';
+            }
+        }
+        str2 = str2 + '\n';
+
+
+}
+console.log(str2);
+
+//Task - 8
+    let str3 = '';
+    for(let i = 1; i < 8; i++) {
+        for(let j = 1; j < 8; j++) {
+            if(i === 1 || (i===j && i < 5)){
+                str3 = str3 + " * ";
+            } else if( i < 5 && (j + i) === 8 ) {
+                str3 = str3 + " * ";
+            } else {
+                str3 = str3 + '   ';
+            }
+        }
+        str3 = str3 + '\n';
+}
+console.log(str3);
+
+//Task - 9
+    let res = '';
+    for(let i = 1; i <= 7; i++) {
+        for(let j = 1; j <= 7; j++) {
+            if(i === 7 || (i===j && i > 3)){
+                res = res + " * ";
+            } else if( i > 3 && (j + i) === 8 ) {
+                res = res + " * ";
+            } else {
+                res = res + '   ';
+            }
+        }
+        res = res + '\n';
+
+}
+console.log(res);
 
